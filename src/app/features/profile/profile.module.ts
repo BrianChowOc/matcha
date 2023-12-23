75a6533fe9f93488d502d9d5b0e9b9dfb0d7e63c
Brian C <brianchow26@gmail.com>
2023-12-23 19:39:04 +0300

    feat: add user profile page

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -5,9 +5,11 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileViewerComponent } from './profile-viewer/profile-viewer.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CheckboxService } from '../connexion/services/checkbox.service';
 
 @NgModule({
   declarations: [ProfileComponent, ProfileViewerComponent],
   imports: [CommonModule, ProfileRoutingModule, SharedModule],
+  providers: [CheckboxService],
 })
 export class ProfileModule {}
--- src/app/features/profile/profile/profile.component.ts
+++ src/app/features/profile/profile/profile.component.ts
@@ -1,10 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Observable, tap } from 'rxjs';
+import { User } from 'src/app/shared/models/user.model';
+import { UserService } from 'src/app/shared/services/user.service';
+import { CheckboxService } from '../../connexion/services/checkbox.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userImageList!: string[];
+  checkboxRows!: { label: string }[][];
+  user$!: Observable<User>;
 
+  picture1Ctrl!: FormControl;
+  picture2Ctrl!: FormControl;
+  picture3Ctrl!: FormControl;
+  picture4Ctrl!: FormControl;
+  pictureControls: FormControl[] = [];
+  pictureForm!: FormGroup;
+
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  informationsForm!: FormGroup;
+
+  interestsForm!: FormGroup;
+  profilForm!: FormGroup;
+
+  biographieCtrl!: FormControl;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private checkboxService: CheckboxService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$ = this.userService.getUser();
+    this.initPictureForm();
+    this.initInterestsForm();
+    this.initBiographieCtrl();
+    this.initInformationsForm();
+    this.initProfilForm();
+    this.checkboxRows = this.checkboxService.checkboxRows;
+  }
+
+  private initPictureForm(): void {
+    this.picture1Ctrl = this.formBuilder.control('');
+    this.picture2Ctrl = this.formBuilder.control('');
+    this.picture3Ctrl = this.formBuilder.control('');
+    this.picture4Ctrl = this.formBuilder.control('');
+
+    this.pictureForm = this.formBuilder.group({
+      picture1: this.picture1Ctrl,
+      picture2: this.picture2Ctrl,
+      picture3: this.picture3Ctrl,
+      picture4: this.picture4Ctrl,
+    });
+
+    this.pictureControls = [
+      this.picture1Ctrl,
+      this.picture2Ctrl,
+      this.picture3Ctrl,
+      this.picture4Ctrl,
+    ];
+  }
+
+  private initInformationsForm(): void {
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control(
+      '',
+      Validators.required
+    );
+    this.informationsForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl,
+    });
+  }
+
+  private initInterestsForm(): void {
+    this.interestsForm = this.formBuilder.group({
+      sciences: [false],
+      photography: [false],
+      cooking: [false],
+      music: [false],
+      diy: [false],
+      sport: [false],
+      reading: [false],
+      paint: [false],
+      theater: [false],
+      games: [false],
+      travel: [false],
+      fashion: [false],
+    });
+  }
+
+  private initProfilForm(): void {
+    this.profilForm = this.formBuilder.group({
+      genre: ['', Validators.required],
+      sexualOrientation: ['', Validators.required],
+      birth: ['', Validators.required],
+      city: ['', Validators.required],
+    });
+  }
+
+  private initBiographieCtrl(): void {
+    this.biographieCtrl = this.formBuilder.control('');
+  }
 }
